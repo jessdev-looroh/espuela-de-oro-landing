1ee9ac11f8922e1cadb42aae46da2c2554e3cd06
jessfigueroa <87987208+jessfigueroa@users.noreply.github.com>
2025-12-17 21:33:53 -0500

    Updates

---
+++ _next/static/9U1XpQaecv8kCoey13QXV/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/ESPUELA_DE_ORO_WEB_APP//_next/:path+",
+        "destination": "/ESPUELA_DE_ORO_WEB_APP/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
